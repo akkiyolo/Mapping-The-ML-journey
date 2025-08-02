5c71db71abc6bae52c080843c29b9288471d8c78
akkiyolo <akshatshukla069@gmail.com>
2025-08-02 17:19:57 +0530

    first commit

---
+++ 12-Logging in Python/logs/logger.py
@@ -0,0 +1,9 @@
+import logging
+logging.getLogger().handlers.clear()  # Clear existing handlers
+logging.basicConfig(
+    filename='app.log',
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
