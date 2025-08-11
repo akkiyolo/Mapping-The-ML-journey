b35df650b983ffeb5cf61adf8aa50fe7d00874e0
akkiyolo <akshatshukla069@gmail.com>
2025-08-11 16:47:55 +0530

    web scraping using threading

--- 13-Multithreading and multiprocessing/13.6-usecase.py
+++ 13-Multithreading and multiprocessing/13.6-usecase.py
@@ -5,3 +5,33 @@ Scenario:Web Scraping
 Web scarping involves making numerous network requests to fetcg web pages . These tasks are I/O bound because they spend a lot of time waiting for responses from server. multithreading can significantly improve the performance by allowing multiple web pages to be fetched concurrently.
 '''
 
+import threading
+import requests
+from bs4 import BeautifulSoup
+
+urls=[
+  'https://python.langchain.com/v0.2/docs/introduction/',
+  'https://python.langchain.com/v0.2/docs/concepts/',
+  'https://python.langchain.com/v0.2/docs/tutorials/'
+]
+
+def fetch_content(url):
+  response=requests.get(url)
+  soup=BeautifulSoup(response.content,'html.parser')
+  print(f'fetched {len(soup.text)} characters from {url}')
+
+
+threads=[]
+
+for url in urls:
+  thread=threading.Thread(target=fetch_content,args=(url,))
+  threads.append(thread)
+  thread.start()
+
+for thread in threads:
+  thread.join()
+
+print("all web pages fetched")
+
+
+
