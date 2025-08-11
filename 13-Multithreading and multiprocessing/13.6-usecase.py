17dbcc5bc0da8599974fccc9ac55962b797fd7cf
akkiyolo <akshatshukla069@gmail.com>
2025-08-11 16:28:15 +0530

    update

---
+++ 13-Multithreading and multiprocessing/13.6-usecase.py
@@ -0,0 +1,7 @@
+'''
+Real world example: multithreading for i/o tasks
+Scenario:Web Scraping
+
+Web scarping involves making numerous network requests to fetcg web pages . These tasks are I/O bound because they spend a lot of time waiting for responses from server. multithreading can significantly improve the performance by allowing multiple web pages to be fetched concurrently.
+'''
+
