03a67e5a28bade94b026f410d16448f1c56e61c1
akkiyolo <akshatshukla069@gmail.com>
2025-09-06 22:40:46 +0530

    http methods

--- 15-Flask/flask/app.py
+++ 15-Flask/flask/app.py
@@ -7,6 +7,13 @@ which will be your WSGI(web server gateway interface) application
 
 app=Flask(__name__)
 
+@app.route("/") ## okay so when the particular route that is / which is for home page is being hit we will be redirected to the home page
+def welcome():
+  return "welcome to this flask app.This should be an amazing course.New changes made, what else could you learn when you got python"
+
+@app.route("/index")
+def hey():
+  return "index is a new route or endpoint of the url"
 
 if __name__=="__main__":
-  app.run()
+  app.run(debug=True) # this command automatically starts the server
